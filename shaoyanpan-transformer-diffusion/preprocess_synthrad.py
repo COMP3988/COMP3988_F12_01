bf1e6e2598e4ab422c80d691a919dfe80af93091
Rick Wang <rickwang121@gmail.com>
2025-10-01 21:50:51 +1300

    fix: preprocess right output format

--- shaoyanpan-transformer-diffusion/preprocess_synthrad.py
+++ shaoyanpan-transformer-diffusion/preprocess_synthrad.py
@@ -3,6 +3,7 @@ Preprocess SynthRAD dataset into MATLAB .mat files
 compatible with Shaoyanpan's transformer-diffusion model
 """
 
+import random
 import os
 import glob
 import argparse
@@ -10,8 +11,12 @@ import SimpleITK as sitk
 import numpy as np
 import scipy.io
 
-def preprocess_synthrad(synthrad_location, out_dir):
+from collections import defaultdict
+
+
+def preprocess_synthrad(synthrad_location, out_dir, seed, proportion):
     os.makedirs(out_dir, exist_ok=True)
+    random.seed(seed)
 
     # TODO can implement more fine-grained control for sections later on
     # TODO masks
@@ -26,41 +31,72 @@ def preprocess_synthrad(synthrad_location, out_dir):
     if len(mri_files) == 0:
         raise RuntimeError(f"No MRI or CT files found!")
 
-    id = 0
-    last_section = ""
+    by_section = defaultdict(list)
 
-    for i, (mri_path, ct_path) in enumerate(zip(mri_files, ct_files), start=1):
-
+    for mri_path in mri_files:
         patient_path = os.path.dirname(mri_path)
         section_path = os.path.dirname(patient_path)
         section = os.path.basename(section_path)
 
-        if section != last_section:
-            last_section = section
-            id = 0
+        by_section[section].append(patient_path)
 
-        case_name = f"{section}_{id}"
-        id += 1
+    train = []
+    val = []
+    test = []
 
-        print(f"[{i}/{len(mri_files)}] Processing {case_name}")
+    for section, patients in by_section.items():
+        patients = sorted(set(patients))
 
-        # Load MRI + CT
-        mri_img = sitk.ReadImage(mri_path)
-        ct_img = sitk.ReadImage(ct_path)
+        if proportion < 1:
+            patients = patients[:int(round(len(patients) * proportion))]
 
-        mri_arr = sitk.GetArrayFromImage(mri_img).astype(np.float32)
-        ct_arr  = sitk.GetArrayFromImage(ct_img).astype(np.float32)
+        random.shuffle(patients)
+        n = len(patients)
 
-        # TODO: add normalization / resampling here if needed
-        # e.g., mri_arr = (mri_arr - np.mean(mri_arr)) / np.std(mri_arr)
+        n_train = int(0.70 * n)
+        n_val = int(0.15 * n)
 
-        # Save as .mat in out_dir
-        out_path = os.path.join(out_dir, f"{case_name}.mat")
+        train.extend(patients[:n_train])
+        val.extend(patients[n_train:n_train + n_val])
+        test.extend(patients[n_train + n_val:])
 
-        scipy.io.savemat(out_path, {
-            "image": mri_arr,
-            "label": ct_arr
-        })
+    pat_i = 1
+
+    def handle_one(type, lst):
+
+        subdir = f'images{type}'
+        os.makedirs(os.path.join(out_dir, subdir), exist_ok=True)
+
+        nonlocal pat_i
+
+        for patient_path in lst:
+
+            patient_code = os.path.basename(patient_path)
+            mr_path = os.path.join(patient_path, "mr.mha")
+            ct_path = os.path.join(patient_path, "ct.mha")
+
+            print(f"[{pat_i}/{len(mri_files)}] Processing {patient_code}")
+            pat_i += 1
+
+            mr_img = sitk.ReadImage(mr_path)
+            ct_img = sitk.ReadImage(ct_path)
+
+            mri_arr = sitk.GetArrayFromImage(mr_img).astype(np.float32)
+            ct_arr  = sitk.GetArrayFromImage(ct_img).astype(np.float32)
+
+            # TODO: add normalization / resampling here if needed
+            # e.g., mri_arr = (mri_arr - np.mean(mri_arr)) / np.std(mri_arr)
+
+            out_path = os.path.join(out_dir, subdir, f"{patient_code}.mat")
+
+            scipy.io.savemat(out_path, {
+                "image": mri_arr,
+                "label": ct_arr
+            })
+
+    handle_one('Tr', train)
+    handle_one('Val', val)
+    handle_one('Ts', test)
 
     print(f"✅ Finished preprocessing. Saved {len(mri_files)} .mat files to {out_dir}")
 
@@ -71,6 +107,10 @@ if __name__ == "__main__":
                         help="Path to SynthRAD dataset root (with imagesTr/, labelsTr/)")
     parser.add_argument("--out_dir", default="SynthRAD",
                         help="Output directory for .mat files")
+    parser.add_argument("--seed", default="42",
+                        help="Seed for how patients are randomly divided into training, validation, and testing sets")
+    parser.add_argument("--proportion", default="1",
+                        help="What proportion of total SynthRAD set to preprocess")
     args = parser.parse_args()
 
-    preprocess_synthrad(args.synthrad_location, args.out_dir)
+    preprocess_synthrad(args.synthrad_location, args.out_dir, args.seed, args.proportion)
